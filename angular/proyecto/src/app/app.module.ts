ea7c01876d0fb2361459f27f70865bf5977d56e6
David Padilla <davidpaag2002@gmail.com>
2022-02-16 21:07:32 +0100

    UPDATE Algunas vistas

--- angular/proyecto/src/app/app.module.ts
+++ angular/proyecto/src/app/app.module.ts
@@ -4,9 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PrincipalComponent } from './components/principal/principal.component';
-import { CrearUsuariosComponent } from './components/crear-usuarios/crear-usuarios.component';
-import { ListarUsuariosComponent } from './components/listar-usuarios/listar-usuarios.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { TallerComponent } from './components/taller/taller.component';
@@ -21,18 +18,15 @@ import { RegistroCocheComponent } from './components/registro-coche/registro-coc
 
 //Definir un array con las rutas que se usarán en la parte dinámica (router-outlet)
 const appRoutes: Routes = [
-  { path: 'Listarusuarios', component:ListarUsuariosComponent },
-  { path: 'Crearusuarios', component:CrearUsuariosComponent },
-  { path: '', redirectTo: '/Listarusuarios', pathMatch: 'full' },
-  { path: '**', component: PrincipalComponent }
+  { path: 'RegistrarCoche', component:RegistroCocheComponent },
+  { path: 'Taller', component:TallerComponent },
+  { path: '', redirectTo: '/Taller', pathMatch: 'full' },
+  { path: '**', component: TallerComponent }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalComponent,
-    CrearUsuariosComponent,
-    ListarUsuariosComponent,
     NavbarComponent,
     TallerComponent,
     FooterComponent,
